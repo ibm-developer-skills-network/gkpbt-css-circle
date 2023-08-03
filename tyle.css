72d552ad49e833680a55a1148ee93f36104d0c70
LikaShl <7121361@gmail.com>
2023-08-03 05:43:31 -0400

    Changing the height and the width of the circle
    Signed-off-by: LikaShl <7121361@gmail.com>

---
+++ tyle.css
@@ -0,0 +1,13 @@
+[1mdiff --git a/style.css b/style.css[m
+[1mindex b1a4e3b..e5b7463 100644[m
+[1m--- a/style.css[m
+[1m+++ b/style.css[m
+[36m@@ -3,6 +3,6 @@[m
+ }[m
+ .circle{[m
+ 			border-radius:50%;[m
+[31m-			width:300px;[m
+[31m-			height:300px;[m
+[32m+[m			[32mwidth:500px;[m
+[32m+[m			[32mheight:500px;[m
+ }[m
